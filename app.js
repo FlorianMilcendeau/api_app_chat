4108f955c12f9c2ce83cb913a08fa120ff77c600
Florian Milcendeau <fmilcendeau85@gmail.com>
2021-02-04 20:14:35 +0100

    Update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const logger = require('morgan');
@@ -23,7 +22,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
 
